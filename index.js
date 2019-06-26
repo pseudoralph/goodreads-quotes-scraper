342d412dd8944e86246cd65dc831ab0ea804a9f1
Ralph Perdomo <ralph@prdmo.com>
2019-06-26 14:11:04 -0700

    fecth data and push as formatted data

--- index.js
+++ index.js
@@ -2,10 +2,36 @@
 
 const cheerio = require('cheerio');
 const request = require('request');
-const url = 'https://www.goodreads.com/quotes/tag/programming';
+const baseUrl = 'https://www.goodreads.com';
+const query = 'programming';
+const url = `${baseUrl}/quotes/tag/${query}`;
+
+const quotesDb = [];
 
 request(url, (err, response, html) => {
   if (!err && response.statusCode === 200) {
-    console.log(html);
+    const $ = cheerio.load(html);
+    const quoteContainers = $('.quoteDetails');
+
+    quoteContainers.each((i, element) => {
+      quotesDb.push({
+        quote: $(element)
+          .find('.quoteText')
+          .text()
+          .replace(/\n\s*/gm, '')
+          .replace(/“|”|\n\s*|―.*$/g, '')
+          .replace(/\n/g, ' '),
+        author: $(element)
+          .find('.authorOrTitle')
+          .text()
+          .replace(/\n\s*/gm, ''),
+        url:
+          baseUrl +
+          $(element)
+            .find('a.smallText')
+            .attr('href')
+      });
+    });
   }
+  console.log(quotesDb);
 });
