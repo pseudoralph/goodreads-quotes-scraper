056a5e46dbc0079c72a0ef163b4eeadb98e96179
Ralph Perdomo <ralph@prdmo.com>
2019-06-27 00:03:03 -0700

    create express server with \quote endpoint

--- index.js
+++ index.js
@@ -1,63 +1,18 @@
-const cheerio = require('cheerio');
-const axios = require('axios');
-const baseUrl = 'https://www.goodreads.com';
-const query = 'philosophy';
-const limitFloor = 30 * 10; // about 30 quotes per page
+const getGoodreadsQuotes = require('./getGoodreadsQuotes');
 
-const quotesDb = [];
+const express = require('express');
+const app = express();
 
-const recursively = (url = `${baseUrl}/quotes/tag/${query}`) => {
-  axios
-    .get(url)
-    .then(response => {
-      if (response.status === 200) {
-        console.log('currently scraping: ' + url);
-        const $ = cheerio.load(response.data);
-        const quoteContainers = $('.quoteDetails');
+app.get('/quotes', (req, res) => {
+  const query = Object.keys(req.query)[0] || 'programming';
+  const sessionData = [];
 
-        quoteContainers.each((i, element) => {
-          quotesDb.push({
-            quote: $(element)
-              .find('.quoteText')
-              .text()
-              .replace(/\n\s*/gm, '')
-              .replace(/“|”|\n\s*|―.*$/g, '')
-              .replace(/\n/g, ' '),
-            author: $(element)
-              .find('.authorOrTitle')
-              .text()
-              .replace(/\n\s*/gm, ''),
-            url:
-              baseUrl +
-              $(element)
-                .find('a.smallText')
-                .attr('href')
-          });
-        });
+  getGoodreadsQuotes(
+    `https://www.goodreads.com/quotes/tag/${query}`,
+    sessionData
+  ).then(response => {
+    res.send(response);
+  });
+});
 
-        if (
-          !$('.next_page').hasClass('disabled') &&
-          quotesDb.length < limitFloor
-        ) {
-          return Promise.resolve({
-            continue: true,
-            payload: baseUrl + $('.next_page').attr('href')
-          });
-        } else {
-          return Promise.resolve({ continue: false, payload: quotesDb });
-        }
-      }
-    })
-    .then(response => {
-      if (response.continue) {
-        recursively(response.payload);
-      } else {
-        console.log(response.payload.length + ' quotes scraped');
-      }
-    })
-    .catch(() => {
-      console.log('error');
-    });
-};
-
-recursively();
+app.listen(3000);
